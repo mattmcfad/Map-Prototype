abadc44048054405f31e4f9cc1cf23f8fad65353
Matt McFadyen <mmcfadyen91@gmail.com>
2015-05-03 16:28:48 -0400

    basic express server online :beer:

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express'),
+	bodyParser = require("body-parser"),
+	fs = require("fs"),
+	multer = require("multer"),
+	app = express();
+
+var	port = process.env.PORT || 8888;
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(multer()); // for parsing multipart/form-data
+
+
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public/layouts"));
+
+
+// Routes
+
+app.use("/", require("./controllers/static"));
+
+
+
+var server = app.listen(port, function() {
+	console.log("listening on localhost:" + port);
+});
